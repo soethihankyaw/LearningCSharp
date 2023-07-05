8e829e14b23ee26f4de5db67d922069de2acf691
SoeThiHanKyaw <soethiha251@gmail.com>
2023-07-05 11:47:43 +0630

    Left commit

--- LearningC#/Program.cs
+++ LearningC#/Program.cs
@@ -10,26 +10,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Arimathic add, sub, addSub;
-
-        add = new Arimathic(Add);
-        sub = new Arimathic(Substract);
-        addSub = add + sub;
-
-        Console.WriteLine($"Add {6} & {4}");
-        add(6, 4);
-        Console.WriteLine($"Substract {6} & {4}");
-        sub(5, 5);
+       
     }
 
-    public delegate void Arimathic(double num1, double num2);
-    public static void Add(double num1, double num2)
-        {
-            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
-        }
-        public static void Substract(double num1, double num2)
-        {
-            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
-        }
+    
 
 }
